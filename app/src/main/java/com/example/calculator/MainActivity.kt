482a8b2cb048bac02af1230f84c245881748a2ea
Dhruv <dhruvagrawal1055@gmail.com>
2022-07-05 22:37:58 +0530

    Added Splash Screen

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.calculator
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#226EE1")))
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity,SimpleActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
